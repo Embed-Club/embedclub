29f2240d585c5a10c828ed66bcce097a54546dbb
Dinaltium <orneryraptor778@gmail.com>
2026-04-09 14:15:10 +0530

    minor fixes

--- src/app/(frontend)/dashboard/title.tsx
+++ src/app/(frontend)/dashboard/title.tsx
@@ -1,14 +1,12 @@
-"use client";
+'use client'
 
-import React from "react";
-import DecryptedTextProps from "@/components/DecryptedText";
+import React from 'react'
+import DecryptedTextProps from '@/components/DecryptedText'
 
 export default function DashboardTitle() {
   return (
     <div className="flex min-h-full w-full flex-col items-center justify-center text-center">
-      <div className="text-2xl md:text-3xl lg:text-4xl font-light mb-4">
-        ELCOE TO
-      </div>
+      <div className="text-2xl md:text-3xl lg:text-4xl font-light mb-4">ELCOE TO</div>
       <div className="text-6xl md:text-7xl lg:text-8xl font-bold">
         <DecryptedTextProps
           text="EBED CLUB"
@@ -19,5 +17,5 @@ export default function DashboardTitle() {
         />
       </div>
     </div>
-  );
+  )
 }
